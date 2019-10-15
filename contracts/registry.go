5b73ee6e4ed071c4047fb9bacd120747bcb902e5
Sergey Bolshchikov <sergey@bolshchikov.net>
2019-10-15 12:57:54 +0300

    change methods in contract

--- contracts/registry.go
+++ contracts/registry.go
@@ -1,21 +1,14 @@
 package main
 
 import (
-	"bytes"
-	"strings"
-
 	"github.com/orbs-network/orbs-contract-sdk/go/sdk/v1"
-	"github.com/orbs-network/orbs-contract-sdk/go/sdk/v1/address"
 	"github.com/orbs-network/orbs-contract-sdk/go/sdk/v1/state"
 )
 
 var SYSTEM = sdk.Export(_init)
-var PUBLIC = sdk.Export(registerMedia, areRegistered)
-
-var OWNER_KEY = []byte("__CONTRACT_OWNER__")
+var PUBLIC = sdk.Export(registerMedia, verifyMedia)
 
 func _init() {
-	state.WriteBytes(OWNER_KEY, address.GetSignerAddress())
 }
 
 func _isRegistered(id string) bool {
@@ -23,22 +16,14 @@ func _isRegistered(id string) bool {
 	return state.ReadString(key) != ""
 }
 
-func areRegistered(ids string) string {
-	res := ""
-	for _, id := range strings.Split(ids, ",") {
-		if _isRegistered(id) {
-			res = res + "1"
-		} else {
-			res = res + "0"
-		}
+func verifyMedia(mediaID string) string {
+	if !_isRegistered(mediaID) {
+		panic("The record with such id does not exist")
 	}
-	return res
+	return state.ReadString([]byte(mediaID))
 }
 
 func registerMedia(mediaID, metadata string) {
-	if !bytes.Equal(state.ReadBytes(OWNER_KEY), address.GetSignerAddress()) {
-		panic("Only contract owner can register media")
-	}
 	key := []byte(mediaID)
 	if _isRegistered(mediaID) {
 		panic("The record already exists")
