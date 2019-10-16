bcb786125b2004b07eaaf96c257f611cea2b5990
Sergey Bolshchikov <sergey@bolshchikov.net>
2019-10-16 11:25:59 +0300

    change package name

---
+++ app/src/main/java/net/sourceforge/openregistrycamera/ToastBoxer.java
@@ -0,0 +1,12 @@
+package net.sourceforge.openregistrycamera;
+
+import android.widget.Toast;
+
+/** Allows methods to update a Toast with a new Toast.
+ */
+public class ToastBoxer {
+    public Toast toast;
+
+    public ToastBoxer() {
+    }
+}
